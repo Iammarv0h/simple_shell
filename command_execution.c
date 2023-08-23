712c8bd0405211ddfede1a8050b3ec8e974bc37d
Iammarv0h <marvinaliero161@gmail.com>
2023-08-23 11:41:33 -0400

    betty compliant command execution

--- command_execution.c
+++ command_execution.c
@@ -2,31 +2,50 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void execmd(char **argv) {
-    if (argv) {
-        char *command = argv[0];
-        char *actual_command = get_location(command);
+/**
+ * execmd - Execute a command with arguments
+ * @argv: An array of strings representing the command and its arguments
+ *
+ * This function executes the specified command along with its arguments.
+ * handles forking child process and executing the command in the child process
+ * The parent process waits for the child process to complete.
+ */
 
-        if (actual_command) {
-            pid_t child_pid = fork();
-            if (child_pid == -1) {
-                perror("Fork failed");
-                exit(EXIT_FAILURE);
-            } else if (child_pid == 0) {
-                /* Child process */
-                execve(actual_command, argv, NULL);
-                perror("Error executing command");
-                exit(EXIT_FAILURE);
-            } else {
-                /* Parent process */
-                int status;
-                waitpid(child_pid, &status, 0);
+void execmd(char **argv)
+{
+	if (argv)
+	{
+		char *command = argv[0];
+		char *actual_command = get_location(command);
 
-                /* Free memory */
-                free(actual_command);
-            }
-        } else {
-            printf("Command not found: %s\n", command);
-        }
-    }
+		if (actual_command)
+		{
+			pid_t child_pid = fork();
+
+			if (child_pid == -1)
+			{
+				perror("Fork failed");
+				exit(EXIT_FAILURE);
+			}
+			else if (child_pid == 0)
+			{
+				/* Child process */
+				execve(actual_command, argv, NULL);
+				perror("Error executing command");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				/* Parent process */
+				int status;
+
+				waitpid(child_pid, &status, 0);
+				/* Free memory */
+				free(actual_command);
+			}
+			else
+			{
+				printf("Command not found: %s\n", command);
+			}
+		}
 }
