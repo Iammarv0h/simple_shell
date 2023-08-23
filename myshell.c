c91559b95da7dfd13e415bc4b11434881c571683
charles-matu <matucharleswachira@gmail.com>
2023-08-23 05:58:31 -0700

    core 234

--- myshell.c
+++ myshell.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "shell.h"
 
 #define MAX_INPUT_SIZE 1024
 
@@ -27,5 +26,6 @@ int main(void)
         }
     }
 
-    return 0;
+    return (0);
 }
+
