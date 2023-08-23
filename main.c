6605aa40991e7ecd0d1148610b3bd32f5ade2f52
Iammarv0h <marvinaliero161@gmail.com>
2023-08-23 11:52:58 -0400

    betty compliant command execution

--- main.c
+++ main.c
@@ -1,35 +1,52 @@
 #include "shell.h"
 
-int main(void) {
-    char *prompt = "(simple_shell $ ";
-    char *lineptr;
-    char *args[MAX_ARGS];
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Description: This function implements a basic Unix shell that reads
+ * commands from the user, executes them, and displays the results.
+ * It provides a prompt, reads user input, processes the input,
+ * and executes the appropriate command.
+ *
+ * Return: Always 0
+ */
 
-    while (1) {
-        int arg_count;
+int main(void)
+{
+	char *prompt = "(simple_shell $ ";
+	char *lineptr;
+	char *args[MAX_ARGS];
 
-        printf("%s", prompt);
-        lineptr = custom_getline();
+	while (1)
+	{
+		int arg_count;
 
-        if (lineptr == NULL) {
-            printf("Exiting shell....\n");
-            return (-1);
-        }
+		printf("%s", prompt);
+		lineptr = custom_getline();
 
-        arg_count = parse_input(lineptr, args);
+		if (lineptr == NULL)
+		{
+			printf("Exiting shell....\n");
+			return (-1);
+		}
 
-        if (arg_count > 0) {
-            if (strcmp(args[0], "setenv") == 0) {
-                shell_setenv(args);
-            } else if (strcmp(args[0], "unsetenv") == 0) {
-                shell_unsetenv(args);
-            } else {
-                execmd(args);
-            }
-        }
-
-        free(lineptr);
-    }
-
-    return (0);
+		arg_count = parse_input(lineptr, args);
+		if (arg_count > 0)
+		{
+			if (strcmp(args[0], "setenv") == 0)
+			{
+				shell_setenv(args);
+			}
+			else if (strcmp(args[0], "unsetenv") == 0)
+			{
+				shell_unsetenv(args);
+			}
+			else
+			{
+				execmd(args);
+			}
+		}
+		free(lineptr);
+	}
+	return (0);
 }
